fca37e979c31b0db2f8d4bf1ad1c0c72d3abb5db
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-04-25 14:32:58 -0700

    Enhance AppHost with Redis, MongoDB, and updated references
    Integrated Redis and MongoDB for improved caching and database management. Updated project references to include the Domain project and added necessary dependencies. Adjusted service configurations to ensure proper initialization and dependencies.

--- TailwindBlog.AppHost/Program.cs
+++ TailwindBlog.AppHost/Program.cs
@@ -1,12 +1,26 @@
+using static TailwindBlog.Domain.Constants.ServiceNames;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.TailwindBlog_ApiService>("apiservice")
-    .WithHttpsHealthCheck("/health");
+var cache = builder.AddRedis(OutputCache);
 
-builder.AddProject<Projects.TailwindBlog_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpsHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
+var mongoServer = builder.AddMongoDB(ServerName)
+		.WithLifetime(ContainerLifetime.Persistent)
+		.WithMongoExpress();
+
+var mongoDb = mongoServer.AddDatabase(DatabaseName);
+
+var apiService = builder.AddProject<Projects.TailwindBlog_ApiService>(ApiService)
+		.WithHttpsHealthCheck("/health")
+		.WithReference(mongoDb)
+		.WaitFor(mongoDb);
+
+builder.AddProject<Projects.TailwindBlog_Web>(WebApp)
+		.WithExternalHttpEndpoints()
+		.WithHttpsHealthCheck("/health")
+		.WithReference(apiService)
+		.WaitFor(apiService)
+		.WithReference(cache)
+		.WaitFor(cache);
 
 builder.Build().Run();

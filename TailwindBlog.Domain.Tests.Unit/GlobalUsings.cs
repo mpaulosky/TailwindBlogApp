7ff5476acc812c49ab95e16b699ab3b1d3f84862
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-04-25 19:24:27 -0700

    Uncomment and consolidate CI steps; add unit tests and dependencies (#5)

---
+++ TailwindBlog.Domain.Tests.Unit/GlobalUsings.cs
@@ -0,0 +1,14 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  TailwindBlog.Domain.Tests.Unit
+// =======================================================
+
+global using System.Diagnostics.CodeAnalysis;
+
+global using JetBrains.Annotations;
+
+global using TailwindBlog.Domain.Constants;

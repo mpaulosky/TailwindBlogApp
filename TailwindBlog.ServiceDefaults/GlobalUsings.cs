cf19399079ae9b4c4bc66a446d92efbe82378889
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-05-12 10:50:30 -0700

    Add GlobalUsings.cs file to centralize global directives
    This file introduces common global using directives for the TailwindBlog.ServiceDefaults project. It improves code maintainability by reducing repetitive using declarations across files.

---
+++ TailwindBlog.ServiceDefaults/GlobalUsings.cs
@@ -0,0 +1,32 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  TailwindBlog.ServiceDefaults
+// =======================================================
+
+#region
+
+global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Net.Http;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+
+global using OpenTelemetry.Metrics;
+global using OpenTelemetry.Resources;
+global using OpenTelemetry.Trace;
+
+#endregion

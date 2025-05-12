bd772a39af38fbdda108fe7ea22e072adb577123
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-05-11 20:50:34 -0700

    Add initial WebProgram.cs file
    Introduced the WebProgram.cs class as part of the TailwindBlog.Web project. This serves as a foundational structure for future functionality within the web application.

---
+++ TailwindBlog.Web/WebProgram.cs
@@ -0,0 +1,14 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name:     WebProgram.cs
+// Project Name:  TailwindBlog.Web
+// =======================================================
+
+namespace TailwindBlog.Web;
+
+public partial class WebProgram
+{
+
+	private static void Run(string[] args) { }
+
+}

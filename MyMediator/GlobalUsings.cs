b93f5a7f5e5af2c7ac5d632ac659a7a2c883148e
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-05-12 10:51:14 -0700

    Add GlobalUsings.cs to centralize global namespace imports
    This new file consolidates commonly used namespaces into one location for the MyMediator project. It simplifies usage across the project by enabling easier and cleaner code management.

---
+++ MyMediator/GlobalUsings.cs
@@ -0,0 +1,20 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : TailwindBlog
+// Project Name :  MyMediator
+// =======================================================
+
+#region
+
+global using System;
+global using System.Linq;
+global using System.Reflection;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.Extensions.DependencyInjection;
+
+#endregion

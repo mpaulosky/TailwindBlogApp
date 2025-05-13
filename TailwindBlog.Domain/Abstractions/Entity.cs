1a33d04916bd52705ba892b701726a52362fd61c
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-05-13 09:45:27 -0700

    Refactor entity properties to use init-only setters.
    Switched mutable setters to init-only setters for `Id` and `CreatedOn` properties to enhance immutability. Adjusted attribute formatting for consistency and readability.

--- TailwindBlog.Domain/Abstractions/Entity.cs
+++ TailwindBlog.Domain/Abstractions/Entity.cs
@@ -13,16 +13,16 @@ public abstract class Entity
 {
 
 	[Key, BsonId, BsonRepresentation(BsonType.ObjectId)]
-	public ObjectId Id { get; set; } = ObjectId.Empty;
+	public ObjectId Id { get; init; } = ObjectId.Empty;
 
-	[Required(ErrorMessage = "A Created On Date is required"),
-	BsonRepresentation(BsonType.DateTime),
-	Display(Name = "Created On")]
-	public DateTime CreatedOn { get; set; } = DateTime.Now;
+	[Required(ErrorMessage = "A Created On Date is required")]
+	[BsonRepresentation(BsonType.DateTime)]
+	[Display(Name = "Created On")]
+	public DateTime CreatedOn { get; init; } = DateTime.Now;
 
-	[BsonElement("modifiedOn"),
-	BsonRepresentation(BsonType.DateTime),
-	Display(Name = "Modified On")]
+	[BsonElement("modifiedOn")]
+	[BsonRepresentation(BsonType.DateTime)]
+	[Display(Name = "Modified On")]
 	public DateTime? ModifiedOn { get; set; }
 
 }

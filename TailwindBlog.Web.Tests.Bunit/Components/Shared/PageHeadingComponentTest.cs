aee027390f8a157a6ab05091a60b7fca0cd8248a
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-06-02 08:09:16 -0700

    Refactor PageHeadingComponent tests for improved structure and validation

--- TailwindBlog.Web.Tests.Bunit/Components/Shared/PageHeadingComponentTest.cs
+++ TailwindBlog.Web.Tests.Bunit/Components/Shared/PageHeadingComponentTest.cs
@@ -18,22 +18,30 @@ public class PageHeadingComponentTest : BunitContext
 {
 
 	[Theory]
-	[InlineData("1", "Test Heading",
-			"<h1 class=\"text-2xl font-bold tracking-tight text-gray-50 py-4\">Test Heading</h1>")]
-	[InlineData("2", "Test Heading",
-			"<h2 class=\"text-3xl font-bold tracking-tight text-gray-50 py-4\">Test Heading</h2>")]
-	[InlineData("3", "Test Heading",
-			"<h3 class=\"text-2xl font-bold tracking-tight text-gray-50 py-4\">Test Heading</h3>")]
+	[InlineData("1", "Test Heading 1", "text-3xl")]
+	[InlineData("2", "Test Heading 2", "text-2xl")]
+	[InlineData("3", "Test Heading 3", "text-1xl")]
 	public void Should_Render_Correct_Heading_Level(string level, string headerText, string expectedHtml)
 	{
+
 		// Arrange & Act
 		var cut = Render<PageHeadingComponent>(parameters => parameters
 				.Add(p => p.Level, level)
 				.Add(p => p.HeaderText, headerText));
-
+		const string expectedHeader =
+				"""
+				<header class="mx-auto max-w-7xl mb-6
+								px-4 py-4 sm:px-4 md:px-6 lg:px-8 
+								rounded-md shadow-md 
+								shadow-blue-500">
+				""";
+		
 		// Assert
 		cut.Markup.Should().Contain(headerText);
+		cut.Markup.Should().Contain($"h{level}");
+		cut.Markup.Should().Contain(expectedHeader);
 		cut.Markup.Should().Contain(expectedHtml);
+		
 	}
 
 }
